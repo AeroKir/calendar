b19d3cb9228f8d1bbe703fecc1643e7295d85be4
Kirill Shevtsov <kirwork.em@gmail.com>
2026-03-10 17:57:02 +0200

    refactoring: eventsStore.ts

--- src/stores/eventsStore.ts
+++ src/stores/eventsStore.ts
@@ -1,51 +1,7 @@
-// import { defineStore } from 'pinia'
-// import type { CalendarEvent } from '@/types/CalendarEvent'
-
-// export const useEventsStore = defineStore('events', {
-//   state: () => ({
-//     events: [] as CalendarEvent[],
-//   }),
-
-//   actions: {
-//     addEvent(event: CalendarEvent) {
-//       this.events.push(event)
-//       this.save()
-//     },
-
-//     updateEvent(updated: CalendarEvent) {
-//       const index = this.events.findIndex((e) => e.id === updated.id)
-
-//       if (index !== -1) {
-//         this.events[index] = {
-//           ...this.events[index],
-//           ...updated,
-//         }
-//       }
-
-//       this.save()
-//     },
-
-//     deleteEvent(id: string) {
-//       this.events = this.events.filter((e) => e.id !== id)
-//       this.save()
-//     },
-
-//     save() {
-//       localStorage.setItem('calendar-events', JSON.stringify(this.events))
-//     },
-
-//     load() {
-//       const data = localStorage.getItem('calendar-events')
-
-//       if (data) {
-//         this.events = JSON.parse(data)
-//       }
-//     },
-//   },
-// })
-
 import { defineStore } from 'pinia'
 import type { CalendarEvent } from '@/types/CalendarEvent'
+
+const STORAGE_KEY = 'calendar-events'
 
 export const useEventsStore = defineStore('events', {
   state: () => ({
@@ -54,15 +10,19 @@ export const useEventsStore = defineStore('events', {
 
   actions: {
     load() {
-      const data = localStorage.getItem('calendar-events')
+      const data = localStorage.getItem(STORAGE_KEY)
 
-      if (data) {
+      if (!data) return
+
+      try {
         this.events = JSON.parse(data)
+      } catch {
+        this.events = []
       }
     },
 
     save() {
-      localStorage.setItem('calendar-events', JSON.stringify(this.events))
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.events))
     },
 
     addEvent(event: CalendarEvent) {
@@ -74,9 +34,9 @@ export const useEventsStore = defineStore('events', {
     updateEvent(updated: CalendarEvent) {
       const index = this.events.findIndex((e) => e.id === updated.id)
 
-      if (index !== -1) {
-        this.events[index] = updated
-      }
+      if (index === -1) return
+
+      this.events[index] = updated
 
       this.save()
     },
